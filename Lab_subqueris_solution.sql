2ef85d1097ec81111833a07179d17dbc8feb8ed0
dianayule <caroyule@hotmail.com>
2026-05-29 10:49:24 +0200

    Lab donev2

--- Lab_subqueris_solution.sql
+++ Lab_subqueris_solution.sql
@@ -27,42 +27,42 @@ WHERE title LIKE 'Alone Trip');
 SELECT f.title FROM sakila.category ca
 JOIN sakila.film_category fc ON ca.category_id=fc.category_id
 JOIN sakila.film f ON fc.film_id=f.film_id
-WHERE ca.category_id = (SELECT ca.category_id FROM sakila.category ca WHERE ca.name LIKE 'family');
+WHERE ca.category_id = (SELECT ca.category_id FROM sakila.category ca WHERE ca.name LIKE '%family%');
 
 -- 5. Retrieve the name and email of customers from Canada using both subqueries and joins. To use joins, 
 -- you will need to identify the relevant tables and their primary and foreign keys.
-
+-- 5. JOIN:
 SELECT cu.first_name AS customer_name, cu.last_name AS customer_last_name, cu.email FROM sakila.country co
 JOIN sakila.city ci ON co.country_id=ci.country_id
 JOIN sakila.address a ON ci.city_id=a.city_id
 JOIN sakila.customer cu ON a.address_id= cu.address_id
-WHERE co.country = ( SELECT co.country FROM sakila.country co WHERE co.country LIKE 'canada');
+WHERE co.country  LIKE '%canada%';
+
+-- 5. subqueries
+SELECT cu.first_name AS customer_name, cu.last_name AS customer_last_name FROM sakila.customer cu
+WHERE cu.address_id IN (
+	SELECT a.address_id FROM sakila.address a
+	WHERE a.city_id IN (
+		SELECT ci.city_id FROM sakila.city ci
+		WHERE ci.country_id IN (
+			SELECT co.country_id FROM sakila.country co
+			WHERE co.country LIKE '%canada%')
+			)
+            );
+
+
 
 -- 6. Determine which films were starred by the most prolific actor in the Sakila database. 
 -- A prolific actor is defined as the actor who has acted in the most number of films. First, 
 -- you will need to find the most prolific actor and then use that actor_id to find the different 
 -- films that he or she starred in.
 
-SELECT actor_id, Num_actors_film FROM (
-	SELECT fa.actor_id, COUNT(fa.film_id) AS Num_actors_film 
-    FROM sakila.film_actor fa
-    GROUP BY fa.actor_id) AS actor_counts
-GROUP BY actor_id
-ORDER BY Num_actors_film DESC
-LIMIT 1;
-
-SELECT f.title FROM sakila.actor a
+SELECT f.title FROM sakila.film f
 JOIN sakila.film_actor fa
-ON a.actor_id=fa.actor_id
-JOIN sakila.film f
-ON fa.film_id=f.film_id
+ON f.film_id=fa.film_id
 WHERE fa.actor_id = (
-	SELECT actor_id 
-    FROM (
-		SELECT fa.actor_id, COUNT(fa.film_id) AS Num_actors_film 
-		FROM sakila.film_actor fa
-		GROUP BY fa.actor_id) AS actor_counts
-		GROUP BY actor_id
-		ORDER BY Num_actors_film DESC
-		LIMIT 1);
-
+	SELECT fa.actor_id
+	FROM sakila.film_actor fa
+	GROUP BY fa.actor_id 
+    ORDER BY COUNT(fa.film_id) DESC LIMIT 1
+	);
