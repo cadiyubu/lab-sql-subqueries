b99c6a41b658cf00067d2e0ed22b3b26149b0ae9
dianayule <caroyule@hotmail.com>
2026-05-29 10:11:27 +0200

    Lab donev1

---
+++ Lab_subqueris_solution.sql
@@ -0,0 +1,68 @@
+USE sakila;
+
+-- 1. Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+SELECT COUNT(title) AS number_of_copies FROM sakila.inventory i
+JOIN sakila.film f
+ON i.film_id=f.film_id
+GROUP BY title
+HAVING title LIKE 'Hunchback Impossible';
+
+-- 2. List all films whose length is longer than the average length of all the films in the Sakila 
+-- database.
+SELECT film_id, title, length FROM sakila.film
+WHERE length > (SELECT AVG(length) FROM sakila.film);
+
+
+
+-- 3. Use a subquery to display all actors who appear in the film "Alone Trip".
+SELECT a.first_name AS Actor_name, a.last_name AS Actor_last_name FROM sakila.film f
+JOIN sakila.film_actor fi ON f.film_id= fi.film_id
+JOIN sakila.actor a ON a.actor_id=fi.actor_id
+WHERE f.film_id = (SELECT film_id FROM sakila.film
+WHERE title LIKE 'Alone Trip');
+
+-- 4. Sales have been lagging among young families, and you want to target family movies for a promotion. 
+-- Identify all movies categorized as family films.
+
+SELECT f.title FROM sakila.category ca
+JOIN sakila.film_category fc ON ca.category_id=fc.category_id
+JOIN sakila.film f ON fc.film_id=f.film_id
+WHERE ca.category_id = (SELECT ca.category_id FROM sakila.category ca WHERE ca.name LIKE 'family');
+
+-- 5. Retrieve the name and email of customers from Canada using both subqueries and joins. To use joins, 
+-- you will need to identify the relevant tables and their primary and foreign keys.
+
+SELECT cu.first_name AS customer_name, cu.last_name AS customer_last_name, cu.email FROM sakila.country co
+JOIN sakila.city ci ON co.country_id=ci.country_id
+JOIN sakila.address a ON ci.city_id=a.city_id
+JOIN sakila.customer cu ON a.address_id= cu.address_id
+WHERE co.country = ( SELECT co.country FROM sakila.country co WHERE co.country LIKE 'canada');
+
+-- 6. Determine which films were starred by the most prolific actor in the Sakila database. 
+-- A prolific actor is defined as the actor who has acted in the most number of films. First, 
+-- you will need to find the most prolific actor and then use that actor_id to find the different 
+-- films that he or she starred in.
+
+SELECT actor_id, Num_actors_film FROM (
+	SELECT fa.actor_id, COUNT(fa.film_id) AS Num_actors_film 
+    FROM sakila.film_actor fa
+    GROUP BY fa.actor_id) AS actor_counts
+GROUP BY actor_id
+ORDER BY Num_actors_film DESC
+LIMIT 1;
+
+SELECT f.title FROM sakila.actor a
+JOIN sakila.film_actor fa
+ON a.actor_id=fa.actor_id
+JOIN sakila.film f
+ON fa.film_id=f.film_id
+WHERE fa.actor_id = (
+	SELECT actor_id 
+    FROM (
+		SELECT fa.actor_id, COUNT(fa.film_id) AS Num_actors_film 
+		FROM sakila.film_actor fa
+		GROUP BY fa.actor_id) AS actor_counts
+		GROUP BY actor_id
+		ORDER BY Num_actors_film DESC
+		LIMIT 1);
+
